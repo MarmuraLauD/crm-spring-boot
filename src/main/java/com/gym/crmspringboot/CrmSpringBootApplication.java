bf4a61d6d44124d362d686f23c20aa46de559fae
LauD <marmuravlad10@gmail.com>
2026-07-16 19:29:58 +0300

    initialize crm-spring-boot project with Gradle and Spring Boot setup

---
+++ src/main/java/com/gym/crmspringboot/CrmSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.gym.crmspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmSpringBootApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(CrmSpringBootApplication.class, args);
+    }
+
+}
